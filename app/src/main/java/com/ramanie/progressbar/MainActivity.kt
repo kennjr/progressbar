3b33f96ac0d6200738a820c8261c4ecefa461c4c
kenedy <kenedy@myhechar.pro>
2022-11-02 20:17:07 +0300

    added the custom progressbar to this activity

--- app/src/main/java/com/ramanie/progressbar/MainActivity.kt
+++ app/src/main/java/com/ramanie/progressbar/MainActivity.kt
@@ -3,41 +3,18 @@ package com.ramanie.progressbar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.ramanie.progressbar.ui.theme.ProgressbarTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ProgressbarTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+                CircularProgressbar(number = 150, percentage = 0.75f)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ProgressbarTheme {
-        Greeting("Android")
     }
 }
